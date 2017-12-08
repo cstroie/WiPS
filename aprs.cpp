276112492f253dd0b3fd16246893ae3d8b09ff02
Costin STROIE <cstroie@bitdefender.com>
2017-12-08 20:10:30 +0200

    Placeholder for APRS.

---
+++ aprs.cpp
@@ -0,0 +1,16 @@
+/**
+  aprs.cpp - Automated Position Reporting System
+
+  Copyright (c) 2017 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of WiFi_APRS_Tracker.
+*/
+
+#include "Arduino.h"
+#include "aprs.h"
+
+APRS::APRS() {
+}
+
+void APRS::init() {
+}
---
+++ aprs.h
@@ -0,0 +1,20 @@
+/**
+  aprs.h - Automated Position Reporting System
+
+  Copyright (c) 2017 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of WiFi_APRS_Tracker.
+*/
+
+#ifndef APRS_H
+#define APRS_H
+
+#include "Arduino.h"
+
+class APRS {
+  public:
+    APRS();
+    void init();
+};
+
+#endif /* APRS_H */
