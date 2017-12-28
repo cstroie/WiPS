4f5da27cd5a8b6239ba7475636dce361fd5e264a
Costin STROIE <cstroie@bitdefender.com>
2017-12-28 18:54:12 +0200

    Status is public.

--- ntp.h
+++ ntp.h
@@ -20,13 +20,13 @@ class NTP {
     void          setServer(const char *server, int port = 123);
     unsigned long timeUNIX(bool sync = true);
     unsigned long uptime(char *buf, size_t len);
+    bool          ntpOk       = false;                  // Flag to know the time is accurate
   private:
     unsigned long ntpSync();
     char          ntpServer[50];                        // NTP server to connect to (RFC5905)
     int           ntpPort     = 123;                    // NTP port
     unsigned long ntpNextSync = 0UL;                    // Next time to syncronize
     unsigned long ntpDelta    = 0UL;                    // Difference between real time and internal clock
-    bool          ntpOk       = false;                  // Flag to know the time is accurate
     int           ntpTZ       = 0;                      // Time zone
 };
 
