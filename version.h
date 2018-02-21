4514f9e61589758af7a79297f635fc7e7b8dd320
Costin STROIE <cstroie@bitdefender.com>
2018-02-21 16:35:24 +0200

    Version up.

--- version.h
+++ version.h
@@ -14,6 +14,6 @@
 // Device specific constants
 const char NODENAME[] = "WiFiTrk";
 const char nodename[] = "wifitrk";
-const char VERSION[]  = "0.2.11";
+const char VERSION[]  = "0.3.1";
 
 #endif /* VERSION_H */
